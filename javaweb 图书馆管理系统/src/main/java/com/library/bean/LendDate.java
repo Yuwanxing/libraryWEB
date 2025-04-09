d8dd920c8ecb03a8dd883a31433ccfb7be31fbd4
YuSihang <valentinehenderson5064@gmail.com>
2025-04-10 03:18:37 +0800

    Initial commit

---
+++ javaweb 图书馆管理系统/src/main/java/com/library/bean/LendDate.java
@@ -0,0 +1,22 @@
+package com.library.bean;
+
+public class LendDate extends Lend {
+    private String lendDateStr;
+    private String backDateStr;
+
+    public String getLendDateStr() {
+        return lendDateStr;
+    }
+
+    public void setLendDateStr(String lendDateStr) {
+        this.lendDateStr = lendDateStr;
+    }
+
+    public String getBackDateStr() {
+        return backDateStr;
+    }
+
+    public void setBackDateStr(String backDateStr) {
+        this.backDateStr = backDateStr;
+    }
+}
---
+++ javaweb 图书馆管理系统/src/main/java/com/library/bean/ReaderInfoDate.java
@@ -0,0 +1,14 @@
+package com.library.bean;
+
+
+public class ReaderInfoDate extends ReaderInfo {
+    String birthday;
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+}
